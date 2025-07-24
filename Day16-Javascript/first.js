d8c19056e2f8ffe7028140b3d2f076312fe7eac4
Bhairab-Nath <nathbhairab22@gmail.com>
2025-07-24 21:55:44 +0545

    Day 16 - JavaScript

---
+++ Day16-Javascript/first.js
@@ -0,0 +1,25 @@
+// Single line comments
+/* Multiple line comments 
+JavaScript is a dynamic, lightweight scripting 
+language developed By Brendan Eich*/
+
+var studentName = "Ashish" //String
+var rollNo = 20 //number
+var gpa = 3.55 //number
+var isStudent = true //boolean
+
+console.log(studentName)
+console.log(typeof(studentName))
+
+console.log(rollNo)
+console.log(typeof(rollNo))
+
+console.log(gpa)
+console.log(typeof(gpa))
+
+console.log(isStudent)
+console.log(typeof(isStudent))
+
+// console.log("Hello World")
+// console.log("JavaScript")
+
